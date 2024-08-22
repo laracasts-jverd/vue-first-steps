ef6abef2c3191264ef03c721e2a0ec91765001fc
joffreyverd <jverd@protonmail.com>
2024-08-22 15:54:45 +0200

    Feature: props for childs & emitting events for parents

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -1,12 +1,14 @@
+import AssignmentCreate from "./AssignmentCreate.js";
 import AssignmentList from "./AssignmentList.js";
 
 export default {
-  components: { AssignmentList },
+  components: { AssignmentList, AssignmentCreate },
 
   template: `
       <section class="space-y-6">
           <assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
           <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
+          <assignment-create @add="add"></assignment-create>
       </section>
   `,
 
@@ -16,7 +18,7 @@ export default {
         { name: 'Finish project', complete: false, id: 1 },
         { name: 'Read Chapter 4', complete: false, id: 2 },
         { name: 'Turn in Homework', complete: false, id: 3 },
-      ]
+      ],
     }
   },
 
@@ -26,6 +28,15 @@ export default {
         inProgress: this.assignments.filter(assignment => !assignment.complete),
         completed: this.assignments.filter(assignment => assignment.complete)
       };
+    },
+  },
+  methods: {
+    add(name) {
+      this.assignments.push({
+        name: name,
+        complete: false,
+        id: this.assignments.length + 1
+      });
     }
   }
 }
